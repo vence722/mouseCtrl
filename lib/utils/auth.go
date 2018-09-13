7d3732eae7b751c97fcf8d8c21d26df78d03cfd5
vence@aftership.com <vence722@users.noreply.github.com>
2018-09-13 14:47:25 +0800

    add connection page

--- lib/utils/auth.go
+++ lib/utils/auth.go
@@ -3,17 +3,19 @@ package utils
 import (
 	"net"
 
+	"github.com/satori/go.uuid"
 	qrcode "github.com/skip2/go-qrcode"
 	"github.com/vence722/convert"
 )
 
-func GenerateQRCode(port int) ([]byte, error) {
-	key := "http://" + getLocalIP() + ":" + convert.Int2Str(port) + "/control"
+func GenerateQRCodeAndAuthToken(port int) ([]byte, string, error) {
+	authToken := uuid.Must(uuid.NewV4()).String()
+	key := "http://" + getLocalIP() + ":" + convert.Int2Str(port) + "/control?token=" + authToken
 	qrCodeData, err := qrcode.Encode(key, qrcode.Medium, 256)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
-	return qrCodeData, nil
+	return qrCodeData, authToken, nil
 }
 
 func getLocalIP() string {
--- lib/web/agent.go
+++ lib/web/agent.go
@@ -11,31 +11,57 @@ import (
 	"mouseCtrl/lib/utils"
 )
 
+// authentication token
+var AuthToken string = ""
+
+// connected flag
+var IsConnected bool = false
+
 func StartAgent(port int) {
-	http.HandleFunc("/", authHandler)
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/qrCode", qrCodeHandler(port))
+	http.HandleFunc("/auth", authHandler)
 	http.HandleFunc("/control", mouseCtrlHandler)
 	http.ListenAndServe(":"+convert.Int2Str(port), nil)
 }
 
-func authHandler(rsp http.ResponseWriter, req *http.Request) {
-	f, _ := os.Open("lib/web/auth.html")
+func indexHandler(rsp http.ResponseWriter, req *http.Request) {
+	f, _ := os.Open("lib/web/index.html")
 	data, _ := ioutil.ReadAll(f)
 	rsp.Write(data)
 }
 
 func qrCodeHandler(port int) func(rsp http.ResponseWriter, req *http.Request) {
 	return func(rsp http.ResponseWriter, req *http.Request) {
-		qrCode, err := utils.GenerateQRCode(port)
+		qrCode, authToken, err := utils.GenerateQRCodeAndAuthToken(port)
 		if err != nil {
 			rsp.Write([]byte(err.Error()))
 			return
 		}
+		// set auth token
+		AuthToken = authToken
 		rsp.Write([]byte(base64.StdEncoding.EncodeToString(qrCode)))
 	}
 }
 
+func authHandler(rsp http.ResponseWriter, req *http.Request) {
+	if IsConnected {
+		rsp.Write([]byte("ok"))
+	} else {
+		rsp.Write([]byte("no"))
+	}
+}
+
 func mouseCtrlHandler(rsp http.ResponseWriter, req *http.Request) {
+	// verify token
+	token := req.URL.Query().Get("token")
+	if token != AuthToken {
+		rsp.Write([]byte("Authentication failed"))
+		rsp.WriteHeader(400)
+		return
+	}
+
+	IsConnected = true
 	f, _ := os.Open("lib/web/mouse_ctrl.html")
 	data, _ := ioutil.ReadAll(f)
 	rsp.Write(data)
